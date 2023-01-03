678340f73b7bff994ebf095510438893f18bf740
ron <ron208888@gmail.com>
2023-01-03 15:52:01 +0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 //地圖
-document.querySelector('a').addEventListener('hover',function(){
-    var theID = document.querySelector(this).attr('id');
-    document.querySelector('select').val(theID);
-    document.querySelector('a').classList.remove('active');
-    document.querySelector(this).classList.add('active');
-})
-  
-document.querySelector('#city').addEventListener('change',function(){
-    var valID = document.querySelector('#city').value;
-    document.querySelector('a').classList.remove('active');
-    document.querySelector('#' + valID).classList.add('active');
-})
-  
+document.querySelector("a").addEventListener("hover", function () {
+  var theID = document.querySelector(this).attr("id");
+  document.querySelector("select").val(theID);
+  document.querySelector("a").classList.remove("active");
+  document.querySelector(this).classList.add("active");
+});
+
+document.querySelector("#city").addEventListener("change", function (event) {
+  var valID = event.target.value;
+  aElements = document.querySelectorAll("a");
+  for (var i = 0; i < aElements.length; i++) {
+    aElements[i].classList.remove("active");
+  }
+  document.querySelector("#" + valID).classList.add("active");
+});
