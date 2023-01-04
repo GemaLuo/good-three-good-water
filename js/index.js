d8cba8b1ccfd4f26f8b6b94d2a1f54bc60c92eb4
ron <ron208888@gmail.com>
2023-01-05 00:15:40 +0800

    add cancel filter fuction

--- js/index.js
+++ js/index.js
@@ -20,17 +20,17 @@ document.querySelector("#city").addEventListener("change", function (event) {
 });
 
 //選單
-document.addEventListener('click', e=>{
-    const isDropdownButton= e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
-    let currentDropdown
-    if (isDropdownButton){
-        currentDropdown=e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
-    }
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown=>{
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active')
-    })
-})
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
